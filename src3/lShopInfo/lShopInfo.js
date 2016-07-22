bc3e3f7a9fd0a2f75d7634f7eb651a9453c7335b
lyx <496670613@qq.com>
2016-07-22 09:51:12 +0800

    细节处理

--- src3/lShopInfo/lShopInfo.js
+++ src3/lShopInfo/lShopInfo.js
@@ -1,10 +1,4 @@
 /**
  * Created by Administrator on 2016/7/18.
  */
-function bindEvents(me) {
-    var $main = $(me.main);
-    $main .on('click', '.serve-more', function() {
-        $('.boutique-list li').removeClass('content-hide');
-        $(this).remove();
-    });
-}
+function bindEvents(me) {}
